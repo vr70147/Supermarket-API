25663892c0beebe02cd5b3933adf1443d5c618bc
Raanan Adut <radesigner84@gmail.com>
2024-11-07 01:02:47 +0200

    Refactor code of reusable utility function for generic Find filtering in database queries

--- src/utils/find-filter.js
+++ src/utils/find-filter.js
@@ -1,31 +1,42 @@
 const filterQuery = async (filter) => {
-  if (filter.columns === undefined && !filter.table) {
-    throw new Error('Missing columns or table');
+  if (!filter.table) {
+    throw new Error('Table name is required');
   }
 
-  const where = filter.where
-    ? `WHERE ${filter.where.column} = ${
-        typeof filter.where.value === 'string'
-          ? `'${filter.where.value}'`
-          : filter.where.value
-      }`
-    : '';
+  if (!Array.isArray(filter.columns)) {
+    throw new Error('Columns must be provided as an array');
+  }
+
+  const columns = filter.columns.length > 0 ? filter.columns.join(', ') : '*';
+
+  let where = '';
+  const values = [];
+
+  if (filter.where && filter.where.column && filter.where.value !== undefined) {
+    where = `WHERE ${filter.where.column} = $1`;
+    values.push(filter.where.value);
+  }
+
   const limit = filter.pageSize !== undefined ? `LIMIT ${filter.pageSize}` : '';
   const offset =
     filter.pageNumber !== undefined && filter.pageSize !== undefined
-      ? `OFFSET ((${filter.pageNumber} - 1) * ${filter.pageSize})`
+      ? `OFFSET ${(filter.pageNumber - 1) * filter.pageSize}`
       : '';
 
+  const orderBy = filter.orderBy
+    ? `ORDER BY ${filter.orderBy} ${filter.sort || 'ASC'}`
+    : '';
+
   const query = `
-    SELECT ${filter.columns ? filter.columns.join() : '*'}
+    SELECT ${columns}
     FROM ${filter.table}
     ${where}
-    ${filter.orderBy ? `ORDER BY ${filter.orderBy} ${filter.sort || ''}` : ''}
+    ${orderBy}
     ${limit}
     ${offset}
-  `;
+  `.trim();
 
-  return query;
+  return { query, values };
 };
 
 module.exports = filterQuery;
